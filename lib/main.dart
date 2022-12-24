045d0d7871f63c8a8fcc67c035f2af87bbc3e30b
Am1r__8 <68626539+CoachAlpha@users.noreply.github.com>
2022-12-23 19:07:55 -0500

    main

--- lib/main.dart
+++ lib/main.dart
@@ -113,3 +113,27 @@ class _SpeechScreenState extends State<SpeechScreen> {
       ),
     );
   }
+
+  void _listen() async {
+    if (!_isListening) {
+      bool available = await _speech.initialize(
+        onStatus: (val) => print('onStatus: $val'),
+        onError: (val) => print('onError: $val'),
+      );
+      if (available) {
+        setState(() => _isListening = true);
+        _speech.listen(
+          onResult: (val) => setState(() {
+            _text = val.recognizedWords;
+            if (val.hasConfidenceRating && val.confidence > 0) {
+              _confidence = val.confidence;
+            }
+          }),
+        );
+      }
+    } else {
+      setState(() => _isListening = false);
+      _speech.stop();
+    }
+  }
+}
